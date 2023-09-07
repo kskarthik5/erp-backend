37dd3c3195b2bdaf3477946ef17ef8a407073abe
Karthik Kaimaparambil <kskarthik20025@gmail.com>
2023-09-07 13:31:34 +0530

    fixed a silly error 3

--- models/databaseUtil.js
+++ models/databaseUtil.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 mongoose.set("strictQuery", false);
 let URI = process.env.DB_URL;
-if (process.env.DB_MODE === "GA") URI = "localhost:27017";
+if (process.env.DB_MODE === "GA") URI = "mongodb://root:toor@mongo:27017/";
 mongoose.connect(URI);
 const connector = mongoose;
 export default connector;
